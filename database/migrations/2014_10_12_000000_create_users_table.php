bd959b0b594ecdcc23746b28417b06e0051bb4a0
devdreamsolution <q3construction1@gmail.com>
2020-11-16 21:20:06 +1000

    change the phone

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,7 +20,7 @@ class CreateUsersTable extends Migration
             $table->string('firstname');
             $table->string('lastname');
             $table->string('hobbies');
-            $table->integer('phone');
+            $table->string('phone');
             $table->integer('country_id');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
